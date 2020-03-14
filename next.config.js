bf8e909386ae8f4f46b79a9f0c45a816886e5e0a
Tucker <trblackw@ncsu.edu>
2020-03-14 11:48:33 -0400

    Add custom marvel font

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 const withCSS = require("@zeit/next-css");
-const withImages = require('next-images');
-const webpack = require('webpack');
-const { parsed: localEnv } = require('dotenv').config();
+const withImages = require("next-images");
+const withFonts = require("next-fonts");
+const webpack = require("webpack");
+const { parsed: localEnv } = require("dotenv").config();
 
-module.exports = withImages(withCSS({
-    webpack(config) {
+module.exports = withImages(
+  withCSS(
+    withFonts({
+      webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
         return config;
-    }
-}));
+      }
+    })
+  )
+);
