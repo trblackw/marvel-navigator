5105335377a734c7c50226856fb63bf007f8a7d2
Tucker <trblackw@ncsu.edu>
2020-03-16 16:29:47 -0400

    Build out character details view

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -3,8 +3,8 @@ import Layout from "../../components/Layout";
 import { NextPage, NextPageContext } from "next";
 import fetch from "isomorphic-unfetch";
 import { Character as CharacterType } from "../../types";
-import { useRouter } from "next/router";
-import { convertTimeStamp, renderImage } from "../../utils";
+import { convertTimeStamp, renderImage, toPascalCase } from "../../utils";
+import Link from "next/link";
 
 interface Props {
   character: CharacterType;
@@ -16,6 +16,7 @@ const Character: NextPage<Props> = ({ character }) => {
       <h3 className="marvel-font uppercase mt-4 text-5xl tracking-wide">
         {character.name}
       </h3>
+
       <div className="container custom-black-bg p-8 rounded-md w-full flex flex-row justify-start">
         {character.thumbnail && (
           <img
@@ -28,12 +29,48 @@ const Character: NextPage<Props> = ({ character }) => {
         )}
         <div className="ml-5 p-2">
           <h3 className="text-lg">
-            <span className="text-gray-400 font-bold">Last updated:</span> {" "}
+            <span className="text-gray-400 font-bold">Last updated:</span>{" "}
             {character.modified && convertTimeStamp(character.modified)}
           </h3>
+          <div className="flex flex-row justify-start items-center">
+            <ul className="pipe-separated-link-list">
+              {character.urls.map(({ type, url }, i) => (
+                <li key={i}>
+                  <a href={url} className="text-sm">
+                    {toPascalCase(type)}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <p className="mt-5 font-thin leading-loose">
+            {character.description}
+          </p>
         </div>
       </div>
-      <p className="px-2 font-thin leading-loose">{character.description}</p>
+      <div className="container mt-6 custom-black-bg p-5 rounded-md">
+        {character.series.available > 0 && (
+          <>
+            <h3 className="text-2xl font-bold text-gray-400 block">
+              Series{" "}
+              <span className="text-sm ml-1 text-red-400">
+                {character.series.available}
+              </span>
+            </h3>
+            <div className="mt-3 ml-3">
+              <ul className="list-none character-detail-list">
+                {character.series.items.map(({ resourceURI, name }, i) => (
+                  <li key={i} className="my-3 text-md">
+                    <Link href={"#"}>
+                      <a>{name}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
+      </div>
     </Layout>
   );
 };
