3f1d9401fd37f1d677c0ea8c4b6ea3c1a93c6414
Tucker <trblackw@ncsu.edu>
2020-03-17 09:50:51 -0400

    Make adjustments to env config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
 const withFonts = require("next-fonts");
 const webpack = require("webpack");
-const { parsed: localEnv } = require("dotenv").config();
+const { parsed: localEnv } = require("dotenv").config({ path: './.env.build'});
 
 module.exports = withImages(
   withCSS(
@@ -10,6 +10,10 @@ module.exports = withImages(
       webpack(config) {
         config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
         return config;
+      },
+      env: {
+        PRIVATE_MARVEL_API_KEY: process.env.PRIVATE_MARVEL_API_KEY,
+        PUBLIC_MARVEL_API_KEY: process.env.PUBLIC_MARVEL_API_KEY
       }
     })
   )
